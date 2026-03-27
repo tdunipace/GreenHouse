a98c09d290c271a0db877c578d078d00e24754d9
taber <tdunipace@lcad.edu>
2026-03-27 13:10:16 -0700

    Plant Script
    Animator is doing fine... script is measuring most things but look direction is fucky.

--- Assets/Scripts/PlantBehavior.cs
+++ Assets/Scripts/PlantBehavior.cs
@@ -1,6 +1,46 @@
 using UnityEngine;
 
-public class PlantBehavior
+public class PlantBehavior: MonoBehaviour
 {
-    
+    public Transform playerCam;
+    public Animator myAnim;
+
+    private Vector3 targetDir;
+    private Vector3 forward;
+    void Update()
+    {
+
+        CheckLooking();
+
+    }
+
+    void CheckLooking()
+    {
+        targetDir = playerCam.position - this.transform.position;
+        forward = playerCam.forward;
+        float angle = Vector3.SignedAngle(targetDir, forward, Vector3.up);
+        if (angle < -5.0F)
+        {
+            //Debug.Log("turn right");
+        }
+        else if (angle > 5.0F)
+        {
+            //Debug.Log("turn left");
+        }
+        else
+        {
+            //Debug.Log("forward");
+        }
+
+            SetAnimations();
+    }
+
+    void SetAnimations()
+    {
+        myAnim.SetFloat("playerDistance", targetDir.magnitude);
+        myAnim.SetFloat("playerLook", forward.magnitude);
+
+        //Debug.Log("Distance is " + targetDir.magnitude);
+        Debug.Log("Look direction is" + forward.magnitude);
+    }
 }
